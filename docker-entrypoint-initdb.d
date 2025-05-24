4d68050fad27d70352ab9ff72e90a7bd5df3295b
Brenda <1010shememing@gmail.com>
2025-05-25 07:32:13 +0800

    debug

---
+++ docker-entrypoint-initdb.d
@@ -0,0 +1,20 @@
+CREATE TABLE menu_items (
+id SERIAL PRIMARY KEY,
+name VARCHAR(255) NOT NULL,
+price DECIMAL(10, 2) NOT NULL,
+image VARCHAR(255)
+);
+
+INSERT INTO menu_items (name, price, image) VALUES
+('Latte', 8.00, 'Latte.jpg'),
+('Cappuccino', 8.00, 'Cappuccino.jpg'),
+('Matcha Latte', 10.00, 'MatchaLatte.jpg'),
+('Iced Americano', 7.00, 'IcedAmericano.jpg'),
+('Hot Chocolate', 7.50, 'HotChocolate.jpg'),
+('Butter Croissant', 5.00, 'Croissant.jpg'),
+('Blueberry Muffin', 6.50, 'BlueberryMuffin.jpg'),
+('Iced Peach Tea, 5.50, 'IcedPeachTea.jpg');
+
+
+
+
